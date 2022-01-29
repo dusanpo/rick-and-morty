90fb75d6dd7e2c500e65a3b805ea7346c27fa9a2
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2022-01-29 14:46:35 +0100

    fetching data from api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "./Home.css";
 
 function Home() {
-  return <div>
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const response = await fetch(
+          "https://rickandmortyapi.com/api/character"
+        );
+        const data = await response.json();
+        //console.log(data);
+        console.log(data.results);
+        
+        setCharacters(data.results);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div>
       <h1>Rick and Morty</h1>
-  </div>;
+      <div>
+        {characters.map(character =>(
+          <article key = {character.id}>
+           <h2>{character.name}</h2>
+           <img src = {character.image} alt = "" />
+          </article>
+        ))}
+      </div>
+
+    </div>
+  );
 }
 
 export default Home;
