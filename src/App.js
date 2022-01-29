c11a66dab3c8de79a0b5a445d0e5bf9a34dcd937
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2022-01-29 19:09:56 +0100

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
-import Home from './components/Home/Home';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
